b3bfff3802b12195b4bf697589522aa955199944
kmhk-naka <phytran35@gmail.com>
2025-03-23 22:50:42 +0900

    Delete redundant load function in +page.ts

--- src/routes/[map]/+layout.server.ts
+++ src/routes/[map]/+layout.server.ts
@@ -1,8 +1,13 @@
 import { maps } from '$lib/data';
+import { error } from '@sveltejs/kit';
 
 export function load({ params }) {
+	const map = maps.find((map) => map.path === params.map);
+
+	if (!map) error(404, 'Map not found!');
+
 	return {
-		map: params.map,
+		map,
 		maps
 	};
 }
