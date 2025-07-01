9dfd21850e9419bf7f4f70d0d4e441f7d8f86842
kmhk-naka <phytran35@gmail.com>
2025-07-02 00:50:55 +0900

    Added testing libraries

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
+import { svelteTesting } from '@testing-library/svelte/vite';
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [sveltekit(), svelteTesting()],
   server: {
     fs: {
       allow: ['styles'],
     },
   },
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./vitest-setup.ts'],
+  },
+  resolve: process.env.VITEST ? { conditions: ['browser'] } : undefined,
 });
